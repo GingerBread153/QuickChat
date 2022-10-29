99348cfc1e7534fea89bbe400c573769072c5b4d
GingerBread <75080001+GingerBread153@users.noreply.github.com>
2022-10-29 16:31:53 +0800

    Finished creating General and Friends Chat

---
+++ app/controllers/quick_devise_controller.rb
@@ -0,0 +1,18 @@
+class QuickDeviseController < ApplicationController
+    class Responder < ActionController::Responder
+        def to_quick_stream 
+            controller.render(options.merge(formats: :html))
+        rescue ActionView::MissingTemplate => e
+            if get?
+                raise e 
+            elsif has_errors? && default_action
+                render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+            else 
+                redirect_to naviation_location
+            end
+        end
+    end
+
+    self.responder = Responder
+    respond_to :html, :quick_stream
+end
